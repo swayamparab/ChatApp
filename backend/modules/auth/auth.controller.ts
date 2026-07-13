4be801835d594dc6347a684f8a1a52e0adaeea77
swayam <swayam.parab2203@gmail.com>
2026-07-13 22:03:31 +0530

    prepare backend for deployment

--- backend/modules/auth/auth.controller.ts
+++ backend/modules/auth/auth.controller.ts
@@ -54,9 +54,12 @@ export async function loginController(req: Request, res: Response) {
         res.cookie("token", token, {
             httpOnly: true,
             secure: process.env.NODE_ENV === "production",
-            sameSite: "lax",
-            maxAge: 7 * 24 * 60 * 60 * 1000
-        })
+            sameSite:
+                process.env.NODE_ENV === "production"
+                    ? "none"
+                    : "lax",
+            maxAge: 7 * 24 * 60 * 60 * 1000,
+        });
 
         return res.status(200).json({
             success: true,
