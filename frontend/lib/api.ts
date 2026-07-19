e67e9a0672262c98a8722e97fe6e33755a2c55d0
swayam <swayam.parab2203@gmail.com>
2026-07-19 19:24:08 +0530

    axios code add support for proxy

--- frontend/lib/api.ts
+++ frontend/lib/api.ts
@@ -1,7 +1,12 @@
 import axios from "axios";
 
+const baseURL =
+    process.env.NODE_ENV === "production"
+        ? "/api"
+        : `${process.env.NEXT_PUBLIC_API_URL}/api`;
+
 export const api = axios.create({
-    baseURL: `${process.env.NEXT_PUBLIC_API_URL}/api`,
+    baseURL,
     withCredentials: true,
     headers: {
         "Content-Type": "application/json",
