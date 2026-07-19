9a89d1dbaf2036b5e2e64e8f386de45daf8cc5a0
swayam <swayam.parab2203@gmail.com>
2026-07-19 19:32:04 +0530

    code fix

--- frontend/lib/socket.ts
+++ frontend/lib/socket.ts
@@ -2,8 +2,8 @@ import { io } from "socket.io-client";
 
 const socketUrl =
     process.env.NODE_ENV === "production"
-        ? window.location.origin
-        : process.env.NEXT_PUBLIC_SOCKET_URL!;
+        ? undefined
+        : process.env.NEXT_PUBLIC_SOCKET_URL;
 
 export const socket = io(socketUrl, {
     withCredentials: true,
